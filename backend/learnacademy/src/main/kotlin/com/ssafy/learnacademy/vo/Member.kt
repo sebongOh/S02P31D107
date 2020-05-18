3fe45363b3c4e60ba7d105f3468c7f81d29fafdf
정종욱 <jjwook1016@gmail.com>
2020-05-18 15:32:29 +0900

    [Add] Entities 추가
    issue: none
    story : none
    - 기본 Entity들 추가
    - 날짜 Base Entity 추가 & 상속

--- backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/vo/Member.kt
+++ backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/vo/Member.kt
@@ -2,6 +2,7 @@ package com.ssafy.learnacademy.vo
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo
 import com.fasterxml.jackson.annotation.ObjectIdGenerators
+import com.ssafy.learnacademy.common.BaseEntity
 import java.util.*
 import javax.persistence.*
 
@@ -11,12 +12,27 @@ class Member (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var memberId: Int? = null,
+
+    @Column(nullable = false)
     var name: String? = null,
+
+    @Column(nullable = false, unique = true)
     var email: String? = null,
+
+    @Column(nullable = false)
     var password: String? = null,
+
+    @Column(nullable = false)
     var address: String? = null,
+
+    @Column(nullable = false)
     var phone: String? = null,
+
+    @Column(nullable = false)
     var age: Int? = null,
+
+    @Column(nullable = false)
     var gender: Byte? = null,
+
     var childId: Int? = null
-)
+) : BaseEntity()
