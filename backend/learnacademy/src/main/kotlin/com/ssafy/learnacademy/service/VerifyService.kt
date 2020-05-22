54d9a8a75d2a4b4b057b18dc425db0c0cd19260d
안경진 <akj0426@naver.com>
2020-05-22 12:11:32 +0900

    [add] 이메일 인증을 위한 dependency 추가, 이메일 인증을 위한 임시 코드 전송하기 기능 구현
    Issue : S02P31D107-99
    Story : S02P31D107-76

---
+++ backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/service/VerifyService.kt
@@ -0,0 +1,57 @@
+package com.ssafy.learnacademy.service
+
+import com.ssafy.learnacademy.repository.VerifyRepository
+import com.ssafy.learnacademy.vo.Verify
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.math.pow
+
+@Service
+class VerifyService(var verifyRepository: VerifyRepository, var mailSender: JavaMailSender) {
+
+    fun getVerify(verifyId: Long): Verify? {
+        return verifyRepository.findByIdOrNull(verifyId)
+    }
+
+    fun getVerifyByEmail(email: String): Verify? {
+        return verifyRepository.findByEmail(email)
+    }
+
+    fun insertVerify(verify: Verify): Verify? {
+        return verifyRepository.save(verify)
+    }
+
+    fun updateVerify(verify: Verify): Verify? {
+        return verifyRepository.save(verify)
+    }
+
+    fun deleteVerify(verifyId: Long) {
+        return verifyRepository.deleteById(verifyId)
+    }
+
+    fun sendEmail(verify: Verify) {
+        val message = SimpleMailMessage()
+        message.setTo(verify.email)
+        message.setSubject("[배울학] 이메일 인증 코드 발송 메일입니다.") // 제목
+        val contents = "회원가입 페이지에서 인증 코드를 입력하세요.\n\ncode : " + verify.code
+        message.setText(contents)
+        mailSender.send((message))
+    }
+
+    fun randomCode(): String? {
+        val certNumLength: Int = 6
+
+        val random: Random = Random(System.currentTimeMillis())
+        val range = 10.0.pow(certNumLength.toDouble()).toInt()
+        val trim = 10.0.pow(certNumLength - 1.toDouble()).toInt()
+
+        var result = random.nextInt(range) + trim
+        if (result > range) {
+            result -= trim
+        }
+        return result.toString()
+    }
+}
