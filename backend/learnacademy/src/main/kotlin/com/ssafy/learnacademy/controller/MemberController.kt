43baca30c2c3e68eccc3a2554997f204e06ca6e7
안경진 <akj0426@naver.com>
2020-05-22 14:51:53 +0900

    [fix] 회원 조회, 삭제 return type ResponseEntity 로 수정

--- backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/controller/MemberController.kt
+++ backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/controller/MemberController.kt
@@ -3,16 +3,25 @@ package com.ssafy.learnacademy.controller
 import com.ssafy.learnacademy.service.MemberService
 import com.ssafy.learnacademy.vo.Member
 import io.swagger.annotations.ApiOperation
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.lang.Exception
 
 @RestController
 @RequestMapping("/member")
 class MemberController(var memberService: MemberService) {
 
+//    @GetMapping("/{memberId}")
+//    @ApiOperation(value = "멤버 찾기", notes = "멤버를 검색합니다")
+//    fun getMember(@PathVariable("memberId") memberId: Long): Member? {
+//        return memberService.getMember(memberId)
+//    }
+
     @GetMapping("/{memberId}")
     @ApiOperation(value = "멤버 찾기", notes = "멤버를 검색합니다")
-    fun getMember(@PathVariable("memberId") memberId: Long): Member? {
-        return memberService.getMember(memberId)
+    fun getMember(@PathVariable("memberId") memberId: Long): ResponseEntity<Member> {
+        var member: Member? = memberService.getMember(memberId) ?: return ResponseEntity.noContent().build()
+        return ResponseEntity.ok().body(member)
     }
 
     // 학원 아이디로 연결된 학원 계정 조회
@@ -28,8 +37,13 @@ class MemberController(var memberService: MemberService) {
     }
 
     @DeleteMapping("/{memberId}")
-    fun deleteMember(@PathVariable("memberId") memberId: Long) {
-        return memberService.deleteMember(memberId)
+    fun deleteMember(@PathVariable("memberId") memberId: Long): ResponseEntity<Unit> {
+        try {
+            memberService.deleteMember(memberId)
+        } catch (e: Exception) {
+            return ResponseEntity.notFound().build()
+        }
+        return ResponseEntity.ok().build()
     }
 
 }
