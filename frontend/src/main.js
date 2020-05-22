56e87c322464976d82f8273b22a2db9b6d14b376
오세봉 <osb4226@naver.com>
2020-05-22 17:51:34 +0900

    [add] 회원가입 중간 단계

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Router from 'vue-router'
-import routes from './routes'
-import store from './vuex/store'
+import Vue from "vue";
+import App from "./App.vue";
+import Router from "vue-router";
+import routes from "./routes";
+import store from "./vuex/store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(Router)
+Vue.use(Router);
+Vue.use(ElementUI);
 
 const router = new Router({
-    routes,
+  routes,
 });
 
 new Vue({
-    router,
-    store,
-    render: h => h(App),
-}).$mount('#app');
-
-
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
