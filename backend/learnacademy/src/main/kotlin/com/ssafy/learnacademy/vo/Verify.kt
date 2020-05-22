ad7f71e1170a3c41698c8636404e4b70d08cfb65
안경진 <akj0426@naver.com>
2020-05-22 11:54:34 +0900

    [fix] verify 객체 수정

--- backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/vo/Verify.kt
+++ backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/vo/Verify.kt
@@ -1,17 +1,18 @@
 package com.ssafy.learnacademy.vo
 
+import com.ssafy.learnacademy.common.BaseEntity
 import javax.persistence.*
 
 @Entity
 @Table(name="verify")
-class Verify {
+class Verify (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var verifyId: Long? = null
+    var verifyId: Long? = null,
 
     @Column(nullable = false)
-    var email: String? = null
+    var email: String? = null,
 
     @Column(nullable = false)
     var code: String? = null
-}
+) : BaseEntity()
