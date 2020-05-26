3fcc42be20d990ba293d3d7ebd08c86b2c0ba358
박정호 <qkrwjdgh0904@naver.com>
2020-05-26 15:44:14 +0900

    [add] 카카오맵 api 연동

--- frontend/src/views/student/components/Search.vue
+++ frontend/src/views/student/components/Search.vue
@@ -1,12 +1,43 @@
 <template>
   <div>
-    <h1>Search</h1>
+    <div id="map" class="map"></div>
+    <div class="list"></div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    window.kakao && window.kakao.maps
+      ? this.initMap()
+      : this.addKakaoMapScript();
+  },
+  methods: {
+    addKakaoMapScript() {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src =
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=be28ab6abd772e2ef55ac5c2f1aa7792";
+      document.head.appendChild(script);
+    },
+    initMap() {
+      let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+      let options = {
+        //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(36.119565, 128.344285), //지도의 중심좌표.
+        level: 5 //지도의 레벨(확대, 축소 정도)
+      };
+
+      let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    }
+  }
+};
 </script>
 
 <style>
+.map {
+  width: 100%;
+  height: 400px;
+}
 </style>
