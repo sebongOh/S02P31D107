493805832648404121cb338767e4f29ade8aa6fa
오세봉 <osb4226@naver.com>
2020-06-03 16:11:50 +0900

    [add] 학원페이지 공지사항 ui

---
+++ frontend/src/views/academy/components/InsertNotice.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-form>
+    <el-form-item label="제목">
+      <el-input v-model="notice.title" />
+    </el-form-item>
+    <el-form-item label="내용">
+      <el-input type="textarea" v-model="notice.content" :rows="7" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="warning" @click="gogo(1)">뒤로가기</el-button>
+      <el-button type="primary" @click="gogo(1)">글쓰기</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notice: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  methods: {
+    gogo(num) {
+      this.$emit("changePageNum", num);
+    },
+  },
+};
+</script>
+
+<style></style>
