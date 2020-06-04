b3e9c2904be23988ad2fa83491f3c6fcfc2f14cf
류가연 <fbrkdus001@naver.com>
2020-06-04 15:41:31 +0900

    [add] 리뷰 작성 추가 Issue : S02P31D107-94 Story : S02P31D107-94

--- frontend/src/views/academyDetail/index.vue
+++ frontend/src/views/academyDetail/index.vue
@@ -1,12 +1,15 @@
 <template>
 <div>
-    <div class="academy-detail">
+    <div class="academy-detail" v-if="!writeReview">
         <h2>{{academyName}}</h2>
         <img v-if="academyImg != ''" class="img-div" :src="academyImg" />
         <div v-if="academyImg == ''" class="img-div"/>
         <div class="detail-div">{{academyDetail}}</div><br>
         <b>주소</b> : {{academyAddress}}<br>
         <b>연락처</b> : {{academyPhone}}<br>
+        <div v-if="isPayed && isAvailable" class="add-review-div">
+            <div class="write-review-div" @click="writeReview = true">리뷰 쓰기</div>
+        </div>
         <div class="review-div">
         <el-carousel arrow="always" height="500px">
             <el-carousel-item v-for="r in reviews" :key="r.reviewId">
@@ -15,20 +18,32 @@
         </el-carousel>
         </div>
     </div>
+    <div v-if="writeReview">
+        <ReviewWrite :academyId="academyId" :email="email" />
+        <table class="btn-table"><tr><td class="btn2"><button class="ok-btn"><b>등록</b></button></td>
+        <td class="btn2"><button class="ok-btn" @click="writeReview = false"><b>취소</b></button></td></tr></table>
+    </div>
     <div class="footer-domain">
         <Footer/>
     </div>
-    <button v-if="isAvailable & isPayed" class="port-btn" @click="goBoard()"><b>게시판<br>이동</b></button>
-    <button v-if="isAvailable & !isPayed" class="port-btn" @click="goPay()"><b>결제하기</b></button>
+    <button v-if="isAvailable & isPayed & !writeReview" class="port-btn" @click="goBoard()"><b>게시판<br>이동</b></button>
+    <button v-if="isAvailable & !isPayed & !writeReview" class="port-btn" @click="goPay()"><b>결제하기</b></button>
 </div>
 </template>
 
 <script>
 import Footer from "@/views/academyDetail/components/Footer";
 import Review from "@/views/academyDetail/components/Review";
+import ReviewWrite from "@/views/academyDetail/components/ReviewWrite";
+import { mapGetters } from "vuex";
+import { getToken } from "@/utils/auth";
+
 export default {
-    components: { Footer, Review },
+    components: { Footer, Review, ReviewWrite },
     props: ["academyId", "name", "address", "phone"],
+    computed: {
+    ...mapGetters(["email"]),
+    },
     mounted(){
         this.getAcademy();
     },
@@ -41,7 +56,8 @@ export default {
             academyPhone: "",
             reviews: {},
             isPayed: false,
-            isAvailable: true
+            isAvailable: true,
+            writeReview: false
         }
     },
     methods: {
@@ -62,7 +78,7 @@ export default {
                 this.academyAddress = "OO시 OO구 OO동";
                 this.academyPhone = "010-0000-1111";
                 this.academyImg = "http://edu.ssafy.com/asset/images/header-logo.jpg";
-                this.reviews = [{ reviewId:1, title:"SSAFY학원 최고! 강추강추! 꼭 와서 들어보세요!", score:5, createDate:"2020-06-04", contents: "가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라s"},
+                this.reviews = [{ reviewId:1, title:"SSAFY학원 최고! 강추강추! 꼭 와서 들어보세요!", score:5, createDate:"2020-06-04", contents: "가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라"},
                 { reviewId:2, title:"SSAFY학원 추천합니다", score:4, createDate:"2020-06-04", contents: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."},
                 { reviewId:3, title:"다시 가고 싶다...", score:5, createDate:"2020-06-04", contents: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."}];
             }else{
@@ -131,5 +147,30 @@ h2{
     width: 100%;
     min-height: 250px;
     height: auto;
+}
+.add-review-div{
+    width: 100%;
+    height: 15px;
+    padding: 2px;
+}
+.write-review-div{
+    float: right;
+    font-size: 12px;
+}
+.btn-table{
+    width: 90%;
+    height: auto;
+    margin: 5%;
+}
+.btn2 {
+  width: 50%;
+  text-align: center;
+}
+.ok-btn {
+  width: 50%;
+  height: 40px;
+  border-radius: 5px;
+  background-color: coral;
+  border: solid 3px chocolate;
 }
 </style>
