0b98ea060e29858bfd91bc4d8920fbddc5a8e96f
안경진 <akj0426@naver.com>
2020-06-07 01:23:46 +0900

    [fix] Token 값이 null 들어왔을 경우 401 error 예외처리

--- backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/Security/JwtAuthenticationFilter.kt
+++ backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/Security/JwtAuthenticationFilter.kt
@@ -9,6 +9,7 @@ import javax.servlet.ServletException
 import javax.servlet.ServletRequest
 import javax.servlet.ServletResponse
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 class JwtAuthenticationFilter(val jwtTokenProvider: JwtTokenProvider) : GenericFilterBean() {
@@ -16,12 +17,18 @@ class JwtAuthenticationFilter(val jwtTokenProvider: JwtTokenProvider) : GenericF
     //request시 들어오는 jwt 필터링 필터체이닝.
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(request: ServletRequest, response: ServletResponse, filterChain: FilterChain) {
-        val token = jwtTokenProvider.resolveToken((request as HttpServletRequest))
-        if (token != null && jwtTokenProvider.valiateToken(token)) {
-            val auth: Authentication = jwtTokenProvider.getAuthentication(token)
-            SecurityContextHolder.getContext().authentication = auth
+        try {
+            val token = jwtTokenProvider.resolveToken((request as HttpServletRequest))
+            if (token != null && jwtTokenProvider.valiateToken(token)) {
+                val auth: Authentication = jwtTokenProvider.getAuthentication(token)
+                SecurityContextHolder.getContext().authentication = auth
+            }
+            filterChain.doFilter(request, response)
+        } catch(e: IllegalStateException) {
+            e.printStackTrace()
+            (response as HttpServletResponse).sendError(401,"Token is null")
         }
-        filterChain.doFilter(request, response)
+
     }
 
 }
