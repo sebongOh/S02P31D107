ed7aedb4fd318162e1a183f8a68ea2c1bf0a48db
오세봉 <osb4226@naver.com>
2020-06-07 20:42:53 +0900

    [add] 학원회원가입 데이터 추가

--- frontend/src/views/join/index.vue
+++ frontend/src/views/join/index.vue
@@ -295,6 +295,7 @@ export default {
       academyName: "",
       academyAddress: "",
       academyPhone: "",
+      academyCategory: "",
       passwordSchema: new PV(),
       passwordConfirm: "",
       isIng: {
@@ -329,8 +330,9 @@ export default {
     rowClicked(row) {
       this.academyName = row.place_name;
       this.academyAddress = row.road_address_name;
-      this.phone = row.phone;
+      this.academyPhone = row.phone;
       this.keyword = row.place_name;
+      this.academyCategory = row.category_name;
       this.academyId = row.id;
       this.searchResult = [];
     },
@@ -394,6 +396,7 @@ export default {
         formData.append("academyName", this.academyName);
         formData.append("academyAddress", this.academyAddress);
         formData.append("academyPhone", this.academyPhone);
+        formData.append("academyCategory", this.academyCategory);
         this.$store
           .dispatch("student/acajoin", formData)
           .then(res => {
