ae4a3c55ed3dbb791b1bf890ac42ce7bd722a252
오세봉 <osb4226@naver.com>
2020-06-07 16:33:27 +0900

    [add] 결제 페이지 수정

--- frontend/src/views/payment/components/PaySucc.vue
+++ frontend/src/views/payment/components/PaySucc.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-card>
+    <el-card style="border:1px solid grey; margin-top:10%">
       <div class="succText">
         <img src="@/assets/images/pay.png" width="50%" height="50%" />
         <h1>결제성공</h1>
@@ -12,22 +12,26 @@
           <div style="float:left">결제번호 :</div>
           <div style="float:right">12312412512</div>
         </div>
-        <br /><br />
+        <br />
+        <br />
         <div class="succInfoCol">
           <div style="float:left">결제시각 :</div>
           <div style="float:right">2020.06.02</div>
         </div>
-        <br /><br />
+        <br />
+        <br />
         <div class="succInfoCol">
           <div style="float:left">학원 :</div>
           <div style="float:right">싸피학원</div>
         </div>
-        <br /><br />
+        <br />
+        <br />
         <div class="succInfoCol">
           <div style="float:left">수강과목 :</div>
           <div style="float:right">싸피2기</div>
         </div>
-        <br /><br />
+        <br />
+        <br />
         <div class="succInfoCol">
           <div style="float:left">가격 :</div>
           <div style="float:right">200,000원</div>
@@ -38,7 +42,9 @@
       <hr />
       <el-row>
         <el-col :span="24">
-          <el-button type="warning" style="width:100%"><b>홈으로</b></el-button>
+          <el-button type="warning" style="width:100%">
+            <b>홈으로</b>
+          </el-button>
         </el-col>
       </el-row>
     </el-card>
@@ -50,9 +56,9 @@ export default {
   data() {
     return {
       payment: {},
-      loading: false,
+      loading: false
     };
-  },
+  }
 };
 </script>
 
