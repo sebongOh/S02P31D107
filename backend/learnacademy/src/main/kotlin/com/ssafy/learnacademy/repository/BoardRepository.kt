5f28e26769b9a85964904745a40c769ffb0e660e
안경진 <akj0426@naver.com>
2020-06-09 00:30:48 +0900

    [add] 학원별 공지사항/자료게시판 등록, 조회

--- backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/repository/BoardRepository.kt
+++ backend/learnacademy/src/main/kotlin/com/ssafy/learnacademy/repository/BoardRepository.kt
@@ -2,7 +2,15 @@ package com.ssafy.learnacademy.repository
 
 import com.ssafy.learnacademy.vo.Board
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface BoardRepository : JpaRepository<Board, Long>
+interface BoardRepository : JpaRepository<Board, Long> {
+
+    @Query("SELECT b FROM Board b INNER JOIN Academy a ON b.academy.academyId = a.academyId WHERE a.academyId = ?1 AND b.type = 0")
+    fun findNoticeAllByAcademyId(academyId: Long): List<Board>?
+
+    @Query("SELECT b FROM Board b INNER JOIN Academy a ON b.academy.academyId = a.academyId WHERE a.academyId = ?1 AND b.type = 1")
+    fun findReferenceAllByAcademyId(academyId: Long): List<Board>?
+}
