955be1c2dc8ac01a865e33b109ce18e440f1d80e
박정호 <qkrwjdgh0904@naver.com>
2020-06-08 11:16:25 +0900

    [add] myPage 디자인 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-    export default {
-        name: 'app',
-    }
+export default {
+  name: "app"
+};
 </script>
+<style>
+#app {
+  font-family: "Yeon Sung", cursive;
+}
+</style>
